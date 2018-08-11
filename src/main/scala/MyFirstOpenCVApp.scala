4c8f0831b47ff67179ea991a3b0e7919766bc35f
Łukasz Wolański <lukasz.wolanski@accenture.com>
2018-08-11 16:49:52 +0200

    Fixed path reading

--- src/main/scala/MyFirstOpenCVApp.scala
+++ src/main/scala/MyFirstOpenCVApp.scala
@@ -1,18 +1,25 @@
+import java.nio.file.Paths
+
 import javax.swing.JFrame
-import org.bytedeco.javacpp.{opencv_core, opencv_imgproc}
-import org.bytedeco.javacpp.opencv_imgcodecs._
-import org.bytedeco.javacv.{CanvasFrame, OpenCVFrameConverter}
 import org.bytedeco.javacpp.opencv_core.Mat
+import org.bytedeco.javacpp.opencv_imgcodecs._
+import org.bytedeco.javacpp.{opencv_core, opencv_imgproc}
+import org.bytedeco.javacv.{CanvasFrame, OpenCVFrameConverter}
 
 object MyFirstOpenCVApp extends App {
-  val path = getClass.getResource("/lena1.png").getPath.tail
-  val img: Mat = imread(path)
+
+  private def printDims(img: Mat) = {
+    println(s"dims: ${img.dims()}, rows: ${img.rows()}, cols: ${img.cols()}, channels: ${img.channels()}")
+  }
+
+  val path = Paths.get(getClass.getResource("/lena1.png").toURI)
+  val img: Mat = imread(path.toString)
   if (img.empty()) {
     // error handling
     // no image has been created...
     // possibly display an error message
     // and quit the application
-    println("Error reading image..." + path)
+    println("Error reading image: " + path)
     System.exit(0)
   }
   printDims(img)
@@ -23,11 +30,6 @@ object MyFirstOpenCVApp extends App {
   printDims(img2)
   val vec = Mat.ones(4,1,opencv_core.CV_8UC1).asMat()
   printDims(vec)
-
-  private def printDims(img: Mat) = {
-    println(s"dims: ${img.dims()}, rows: ${img.rows()}, cols: ${img.cols()}, channels: ${img.channels()}")
-  }
-
   val canvas = new CanvasFrame("My Image", 1)
 
   // Request closing of the application when the image window is closed
